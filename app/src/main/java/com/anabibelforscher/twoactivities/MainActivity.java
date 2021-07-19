3d2c97e86ee0f111e718921f91a65c28a560a097
anabibelforscher <anaolivercontact@gmail.com>
2021-07-19 14:12:05 -0300

    Saving and restoring the Activity instance state

--- app/src/main/java/com/anabibelforscher/twoactivities/MainActivity.java
+++ app/src/main/java/com/anabibelforscher/twoactivities/MainActivity.java
@@ -33,6 +33,17 @@ public class MainActivity extends AppCompatActivity {
         mMessageEditText = findViewById(R.id.editText_main);
         mReplyHeadTextView = findViewById(R.id.text_header_reply);
         mReplyTextView = findViewById(R.id.text_message_reply);
+
+        if (savedInstanceState != null){
+            boolean isVisible =
+                    savedInstanceState.getBoolean("reply_visible");
+            if (isVisible){
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(savedInstanceState.getString("reply_text"));
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
+
     }
 
     @Override
@@ -63,6 +74,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d(LOG_TAG,"onDestroy");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mReplyHeadTextView.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("reply_visible", true);
+            outState.putString("reply_text",
+                    mReplyTextView.getText().toString());
+        }
     }
 
     public void launchSecondActivity(View view) {
